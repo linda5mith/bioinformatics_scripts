84218e77f44b4a043b9fdbb962b9dea933f5bb03
Linda Smith <87448361+linda5mith@users.noreply.github.com>
2022-08-25 17:47:37 +0100

    Update parse_genbank_by_product_name.py

--- parse_genbank_by_product_name.py
+++ parse_genbank_by_product_name.py
@@ -5,9 +5,9 @@ gb_file = "PROKKA_08252022/PROKKA_08252022.gbk"
 
 features=[]
 for record in SeqIO.parse(open(gb_file,"r"), "genbank"):
-    for f in record.features:
-        if f.type == 'CDS':
-            product=(f.qualifiers.get('product')[0].split('[')[0].strip())
+    for feature in record.features:
+        if feature.type == 'CDS':
+            product=(feature.qualifiers.get('product')[0].split('[')[0].strip())
             #print(product)
             if 'DNA polymerase' in product:
-                print(f.seq)
+                print(feature)
