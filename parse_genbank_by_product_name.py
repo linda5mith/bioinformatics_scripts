ac76c8ff635a7e9f0772f1a5afb3d60a41fe1ed4
Linda Smith <87448361+linda5mith@users.noreply.github.com>
2022-08-26 10:28:35 +0100

    writing results to file

--- parse_genbank_by_product_name.py
+++ parse_genbank_by_product_name.py
@@ -1,13 +1,20 @@
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 import re
 
 gb_file = "PROKKA_08252022/PROKKA_08252022.gbk"
 
-features=[]
-for record in SeqIO.parse(open(gb_file,"r"), "genbank"):
-    for feature in record.features:
-        if feature.type == 'CDS':
-            product=(feature.qualifiers.get('product')[0].split('[')[0].strip())
-            #print(product)
-            if 'DNA polymerase' in product:
-                print(feature)
+with open('DNA_polymerase_crAss.fasta','w') as handle:
+    for record in SeqIO.parse(open(gb_file,"r"), "genbank"):
+        for feature in record.features:
+            if feature.type == 'CDS':
+                product=(feature.qualifiers.get('product')[0].split('[')[0].strip())
+                if 'DNA polymerase' in product:
+                    #print(feature.qualifiers.get('product')[0])
+                    #print(feature)
+                    header=(f'{record.id} {product} {feature.location}')
+                    nt_seq=(feature.extract(record.seq))
+                    SeqIO.write(SeqRecord(record.seq,id=header,description=''),handle,'fasta')
+    handle.close()
+    print('Done')
+
