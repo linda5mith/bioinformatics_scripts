4f3224b430df6591d2297910ecd713857f0c7699
Linda Smith <87448361+linda5mith@users.noreply.github.com>
2022-12-16 17:05:38 +0000

    Update process_blast.py

--- process_blast.py
+++ process_blast.py
@@ -1,2 +1,3 @@
+df.sort_values(by=['qseqid','qseqid','sstart','send'],inplace=True)
 s = df.groupby(['qseqid', 'sseqid'])['send'].shift()
 df['length'] = df['sstart'].lt(s) * df['sstart'].sub(s.fillna(0)) + df['length']
