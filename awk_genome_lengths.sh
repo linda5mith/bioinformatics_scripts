c0b5c0002a523d176b09b16a1f373b1ae3e15a6f
Linda Smith <87448361+linda5mith@users.noreply.github.com>
2023-02-16 14:48:32 +0000

    Update awk_genome_lengths.sh

--- awk_genome_lengths.sh
+++ awk_genome_lengths.sh
@@ -1,3 +1,12 @@
-awk '/^>/ {if (seqlen){print seqlen}; printf$0"," ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' pooled_translations.faa > pooled_genome_lengths.txt
+# $1 = path to fasta file
 
-sed -i s'/>//g' pooled_genome_lengths.txt
+name=$(basename "$1" .fasta)
+echo $1
+echo "$name"
+
+awk '/^>/ {if (seqlen){print seqlen}; printf$0"," ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' $1 > "$name"_lengths.txt
+
+sed -i s'/>//g' "$name"_lengths.txt
+
+cat "$name"_lengths.txt | rev | sed "s/,/:/2g" | sed 's/://' | rev > "$name"_clean.csv
+rm "$name"_lengths.txt && mv "$name"_clean.csv "$name"_lengths.txt
